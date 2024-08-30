11de796a2d87b6f59a27c85a3baf5c8e6f0662f6
manideep-udumula <ruhithsabir@gmail.com>
2024-08-30 16:46:03 +0530

    first commit

---
+++ Helloworld/Helloworld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.24;
+
+contract Helloworld {
+    function helloworld()external pure returns(string memory){
+        return "Hello World";
+    }
+}
